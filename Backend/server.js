49ccf1bd625477a5be3fc996576d762b486f349f
Sahana-DS <sahana2000ds@gmail.com>
2025-06-20 20:19:10 +0530

    sahana-leave-module

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const { Pool } = require('pg');
 const cors = require('cors');
 const app = express();
-const port = 3001;
+const port = 3000;
 
 // PostgreSQL database configuration
 const pool = new Pool({
